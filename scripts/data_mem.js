57c6a8cb2a44f38c4ba3f48159b94b84cc62cfbe
Mairans <142796352+Mairans@users.noreply.github.com>
2024-03-14 00:54:07 +0300

    Add files via upload

---
+++ scripts/data_mem.js
@@ -0,0 +1,34 @@
+export const mems = [
+    {
+        name: 'harry',
+        src: 'images/mem.png'
+    },
+    {
+        name: 'harry',
+        src: 'images/harry.png'
+    },
+    {
+        name: 'hitler',
+        src: 'images/hitler.png'
+    },
+    {
+        name: 'harry',
+        src: 'images/itachi.png'
+    },
+    {
+        name: 'harry',
+        src: 'images/minion.png'
+    },
+    {
+        name: 'harry',
+        src: 'images/spooder.png'
+    },
+    {
+        name: 'harry',
+        src: 'images/vas.png'
+    },
+    {
+        name: 'rus',
+        src: 'images/rys.jpg'
+    }
+];
